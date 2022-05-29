37ab7cd8e0ff5c8199006e0ec02346d89d5cd254
patricia <pnfreitas2014@gmail.com>
2022-05-28 22:49:32 -0300

    feat: Adiciona o model Veiculo

---
+++ aula-backend/Models/Veiculo.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace aula_backend.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Obrigatório informar o nome!")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a Placa!")]
+        public string Placa { get; set; }
+    }
+}
