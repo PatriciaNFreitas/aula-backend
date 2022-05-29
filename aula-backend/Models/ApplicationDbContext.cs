133cd5dda0cf88b2db170b1eabbe97a7d580ed33
patricia <pnfreitas2014@gmail.com>
2022-05-28 23:25:08 -0300

    feat:Configurações do Entity Framework

---
+++ aula-backend/Models/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace aula_backend.Models
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Veiculo> Veiculos { get; set; }
+    }
+}
